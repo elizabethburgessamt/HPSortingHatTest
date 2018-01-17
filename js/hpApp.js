86f5cd71eb391570117b42266011a261952686a3
Silver Dragon <silverdragon013@hotmail.com>
2018-01-16 19:30:45 -0500

    Refactored the JSON questions file so it could be read by the app

--- js/hpApp.js
+++ js/hpApp.js
@@ -1,10 +1,13 @@
 var app = angular.module("hpApp", []);
 
-app.controller("SortingHatController", function($scope) {
+app.controller("SortingHatController", function($scope, $http) {
 	$scope.gPoints = 0;
 	$scope.sPoints = 0;
 	$scope.rPoints = 0;
 	$scope.hPoints = 0;
 	
-	
+	$http.get("questions.json").then(function(response) {
+        $scope.questions = response.data.questions;
+        $scope.options = response.data.questions.query.options;
+    });
 });
